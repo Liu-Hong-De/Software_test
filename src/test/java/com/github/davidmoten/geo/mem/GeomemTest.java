d8afec644293503f7555ad4dbd410335d63860b5
Liu-Hong-De <u10416012@go.utaipei.edu.tw>
2020-03-23 15:01:11 +0800

    GeomemTest add and modify document

--- src/test/java/com/github/davidmoten/geo/mem/GeomemTest.java
+++ src/test/java/com/github/davidmoten/geo/mem/GeomemTest.java
@@ -4,10 +4,6 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Iterator;
-
 import static org.junit.Assert.*;
 
 public class GeomemTest {
@@ -25,9 +21,14 @@ public class GeomemTest {
 
     @Test
     public void testFind() {
-        Iterable<Info<Integer, Integer>> it = Collections.emptyList();
-        geomem.add(4.92187500, 4.92187500, 3, 18);  // sog
+        geomem.add(4.92187500, 4.92187500, 12, 18);  // sog
+        assertEquals("[]", geomem.find(6, 4, 4, 6, 0, 10).toString());
+
+        geomem.add(4.921875, 4.921875, 3, 18);
         assertEquals("[Info [lat=4.921875, lon=4.921875, time=3, value=18, id=Optional.of(18)]]", geomem.find(6, 4, 4, 6, 0, 10).toString());
+
+        geomem.add(7.99804688, 4.04296875, 2, 10, 2);   // s16v
+        assertEquals("[Info [lat=7.99804688, lon=4.04296875, time=2, value=10, id=Optional.of(2)]]", geomem.find(10, 2, 6, 6, 0, 5).toString());
     }
 
 //    @Test
