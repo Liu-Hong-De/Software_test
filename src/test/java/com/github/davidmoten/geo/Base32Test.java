ff3e50be216b53fa41d69a7b500eed5ee625a9a3
Liu-Hong-De <U10416012@go.utaipei.edu.tw>
2020-04-23 17:07:42 +0800

    add test with ISP in Base32Test initial

--- src/test/java/com/github/davidmoten/geo/Base32Test.java
+++ src/test/java/com/github/davidmoten/geo/Base32Test.java
@@ -62,4 +62,90 @@ public class Base32Test {
         String s = Base32.padLeftWithZerosToLength("29jw", 5);
         assertEquals("029jw", s);
     }
+
+//    test encodeBase32(long i, int length) with ISP
+    @Test
+    public void testEncodeBase32WithISP() throws Exception {
+//        i <= -32, length < 0
+        String encodehash = Base32.encodeBase32(-35, -2);
+        assertEquals("-13", encodehash);
+
+//        -32 < i < 32, length < 0
+        encodehash = Base32.encodeBase32(-10, -2);
+        assertEquals("-b", encodehash);
+
+//        i >= 32, length < 0
+        encodehash = Base32.encodeBase32(35, -2);
+        assertEquals("13", encodehash);
+
+//        i <= -32, length = 0
+        encodehash = Base32.encodeBase32(-35, 0);
+        assertEquals("-13", encodehash);
+
+//        -32 < i < 32, length = 0
+        encodehash = Base32.encodeBase32(-10, 0);
+        assertEquals("-b", encodehash);
+
+//        i >= 32, length = 0
+        encodehash = Base32.encodeBase32(35, 0);
+        assertEquals("13", encodehash);
+
+//        i <= -32, length > 0
+        encodehash = Base32.encodeBase32(-35, 5);
+        assertEquals("-00013", encodehash);
+
+//        -32 < i < 32, length > 0
+        encodehash = Base32.encodeBase32(-10, 5);
+        assertEquals("-0000b", encodehash);
+
+//        i >= 32, length > 0
+        encodehash = Base32.encodeBase32(35, 5);
+        assertEquals("00013", encodehash);
+    }
+
+//    test encodeBase32(long i) with ISP
+    @Test
+    public void testEncodeBase32WithISP_2() throws Exception {
+//        i <= -32
+        String encodeHash = Base32.encodeBase32(-33);
+        assertEquals("-000000000011",encodeHash);
+
+//        -32 < i < 32
+        encodeHash = Base32.encodeBase32(-10);
+        assertEquals("-00000000000b", encodeHash);
+
+//        i >= 32
+        encodeHash = Base32.encodeBase32(33);
+        assertEquals("000000000011", encodeHash);
+    }
+
+//    test decodeBase32(String hash) with ISP
+//    C1：hash start with "-", C2：hash contains the character which can not be converted in Base 32
+    @Test
+    public void testDecodeBase32WithISP() throws Exception {
+        long decodeHash;
+//        C1：T, C2：T
+        try {
+            decodeHash = Base32.decodeBase32("-**83");
+            fail("no exception");
+        } catch (Exception message) {
+            assertTrue(message.getMessage().contains("not a base32 character: *"));
+        }
+
+//        C1：T, C2：F
+        decodeHash = Base32.decodeBase32("-29jw");
+        assertEquals(-75324, decodeHash);
+
+//        C1：F, C2：T
+        try {
+            decodeHash = Base32.decodeBase32("**83");
+            fail("no exception");
+        } catch (Exception message) {
+            assertTrue(message.getMessage().contains("not a base32 character: *"));
+        }
+
+//        C1：F, C2：F
+        decodeHash = Base32.decodeBase32("29jw");
+        assertEquals(75324, decodeHash);
+    }
 }
